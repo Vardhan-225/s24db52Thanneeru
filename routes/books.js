b66fcf59ece625a676da5ea186d12cb4d05df78d
Akash Thanneeru <iamakash225@gmail.com>
2024-04-03 21:09:50 -0500

    address uncomitted changes

--- routes/books.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET books page. */
-router.get('/', function(req, res, next) {
-  res.render('books', { title: 'Search Results - Books' });
-});
-
-module.exports = router;
