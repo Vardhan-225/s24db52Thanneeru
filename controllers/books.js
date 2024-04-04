514855bf36644e8cc8508c3655963827d1475697
Akash Thanneeru <iamakash225@gmail.com>
2024-04-03 22:53:26 -0500

    Implemented list all functions in books controller, updated books list function

--- controllers/books.js
+++ controllers/books.js
@@ -1,5 +1,15 @@
 const Book = require('../models/books');
 
+// Display all books view
+exports.books_view_all_Page = async function(req, res) {
+    try {
+        const books = await Book.find();
+        res.render('books', { title: 'Book Search Results', results: books });
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
 // List all books
 exports.books_list = async function(req, res) {
     try {
@@ -16,8 +26,23 @@ exports.books_detail = function(req, res) {
 };
 
 // Handle book create on POST
-exports.books_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book create POST');
+exports.books_create_post = async function(req, res) {
+    console.log(req.body);
+    let document = new Book();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"title":"The Alchemist", "author":"Paulo Coelho", "year":1988}
+    document.title = req.body.title;
+    document.author = req.body.author;
+    document.year = req.body.year;
+    try {
+        let result = await document.save();
+        res.send(result);
+    } catch(err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
 // Handle book delete on DELETE
--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const booksController = require('../controllers/books');
+const books = require('../controllers/books');
 
 // GET request for displaying all books
-router.get('/', booksController.books_list);
+router.get('/', books.books_view_all_Page);
 
 // POST request for creating a new book
-router.post('/', booksController.books_create_post);
+router.post('/', books.books_create_post);
 
 // DELETE request for deleting a book
-router.delete('/:id', booksController.books_delete);
+router.delete('/:id', books.books_delete);
 
 // PUT request for updating a book
-router.put('/:id', booksController.books_update_put);
+router.put('/:id', books.books_update_put);
 
 // GET request for displaying details of a specific book
-router.get('/:id', booksController.books_detail);
+router.get('/:id', books.books_detail);
 
 module.exports = router;
