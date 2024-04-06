40ac2fcbac2377dd968ed80e427a06ccdcaa2ed0
Akash Thanneeru <iamakash225@gmail.com>
2024-04-05 20:55:20 -0500

    Revert "added buttons"
    This reverts commit 1789f995da97946ba8c41fb3877aa273e389bbdd.

--- controllers/books.js
+++ controllers/books.js
@@ -28,26 +28,29 @@ exports.books_detail = function(req, res) {
 // Handle book create on POST
 exports.books_create_post = async function(req, res) {
     console.log(req.body);
-    let document = new Book(req.body);
+    let document = new Book();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"title":"The Alchemist", "author":"Paulo Coelho", "year":1988}
+    document.title = req.body.title;
+    document.author = req.body.author;
+    document.year = req.body.year;
     try {
         let result = await document.save();
         res.send(result);
     } catch(err) {
-        res.status(500).send({ error: err.message });
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
 };
 
 // Handle book delete on DELETE
-exports.books_delete = async function(req, res) {
-    try {
-        const deletedBook = await Book.findByIdAndDelete(req.params.id);
-        res.send(deletedBook);
-    } catch (err) {
-        res.status(500).send({ error: err.message });
-    }
+exports.books_delete = function(req, res) {
+    res.send('NOT IMPLEMENTED: Book delete DELETE ' + req.params.id);
 };
 
 // Handle book update on PUT
-exports.books_update_put = async function(req, res) {
+exports.books_update_put = function(req, res) {
     res.send('NOT IMPLEMENTED: Book update PUT' + req.params.id);
 };
