cf1b952da5b928df65e382120315d6eb9789e09a
Akash Thanneeru <iamakash225@gmail.com>
2024-04-13 22:28:53 -0500

    Added authentication to secure update page

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,15 @@ var express = require('express');
 const books_controlers = require('../controllers/books');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
 // GET all books and render view
 router.get('/', books_controlers.books_view_all_Page);
 
@@ -10,7 +19,7 @@ router.get('/detail', books_controlers.books_view_one_Page);
 router.get('/create', books_controlers.books_create_Page);
 
 /* GET create update page */
-router.get('/update', books_controlers.books_update_Page);
+router.get('/update', secured, books_controlers.books_update_Page);
 
 /* GET delete costume page */ 
 router.get('/delete', books_controlers.books_delete_Page); 
